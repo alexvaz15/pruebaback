cace72e40873a4fcde4961e3e2559d4196d36a3d
Julian Alexander <191233@ids.upchiapas.edu.mx>
2020-11-06 21:17:07 -0600

    Soluciones de errores

--- Profile/serializers.py
+++ Profile/serializers.py
@@ -5,4 +5,4 @@ from Profile.models import ProfileModel
 class ProfileSerialiezers(serializers.ModelSerializer):
     class Meta:
         model = ProfileModel
-        fileds = ('__all__')
+        fields = ('__all__')
