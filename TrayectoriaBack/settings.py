f85632883371067fb29309e7e8b1a4d613d93db8
alexvaz15 <191233@ids.upchiapas.edu.mx>
2020-12-12 09:03:21 -0600

    Add conection database

--- TrayectoriaBack/settings.py
+++ TrayectoriaBack/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
         'NAME':'webdb',
         'USER': 'webuserdb',
         'PASSWORD':'web141189', 
-        'HOST':'34.227.83.246',
+        'HOST':'34.227.83.246',#ip public
         'PORT': '5432'
 
     }
