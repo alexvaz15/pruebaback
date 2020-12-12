0a02124add19c872795f4b5e22a6ce841963e9cc
alexvaz15 <191233@ids.upchiapas.edu.mx>
2020-12-12 12:06:39 -0600

    Changes in the settings

--- TrayectoriaBack/settings.py
+++ TrayectoriaBack/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
         'NAME':'webdb',
         'USER': 'webuserdb',
         'PASSWORD':'web141189', 
-        'HOST':'34.227.83.246',
+        'HOST':'losthost',
         'PORT': '5432'
 
     }
